b8a50bcf5949261b54852a809a6adafd1a05396f
Abby <harbeyteejay@gmail.com>
2022-10-28 09:49:59 +0100

    adding share buttons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import pic from '../assets/pic.svg'
+import shareIcon from '../assets/share-icons.svg'
+import shareDots from '../assets/share-dots.svg'
 export default function Profile() {
   return (
     <div className='profile'>
         <img src={pic} alt="a profile picture" id='profile__img' />
         <span id="twitter">@Abbycodess</span>
+        <div className='share-btn'>
+          <img src={shareDots} alt="share button" className="share-dot" />
+          <img src={shareIcon} alt="share button" className="share-icon" />
+        </div>
         {/*<span id="slack">@Abbytee</span>*/}
     </div>
   )
