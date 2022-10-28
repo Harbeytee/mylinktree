afccdadcbb1dbc89a2d2f08a41abcea81219268a
Abby <harbeyteejay@gmail.com>
2022-10-28 15:54:58 +0100

    added title

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -7,19 +7,19 @@ export default function Links() {
 
         <a id="btn__zuri" href="https://training.zuri.team/">zuri team</a>
 
-        <a id="books" href="http://books.zuri.team">zuri books</a>
-        <p>Click the link above to get handpicked books and customized guides about design and coding to help you grow and advance your tech careers.</p>
+        <a title="Click the link to get handpicked books and customized guides about design and coding to help you grow and advance your tech careers." id="books" href="http://books.zuri.team">zuri books</a>
+        
 
-        <a id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=Abbytee">python books</a>
-        <p>Click the link above if you want a book that takes you from a beginner python developer to an advanced python developer. The books are very beginner friendly and contain real world examples to help you along the way</p>
-
-
-        <a id="pitch" href="https://background.zuri.team">background check for coders</a>
-        <p>Click on the link above for a service that runs background checks on coders</p>
+        <a title='Click the link if you want a book that takes you from a beginner python developer to an advanced python developer. The books are very beginner friendly and contain real world examples to help you along the way' id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=Abbytee">python books</a>
+        
 
 
-        <a id="book_design" href="https://books.zuri.team/design-rules">design books</a>
-        <p>Click on the link above to get the free Design Rules Book by Zuri that gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.</p>
+        <a title="Click on the link for a service that runs background checks on coders" id="pitch" href="https://background.zuri.team">background check for coders</a>
+        
+
+
+        <a title='Click on the link to get the free Design Rules Book by Zuri that gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.' id="book_design" href="https://books.zuri.team/design-rules">design books</a>
+        
     </div>
   )
 }
